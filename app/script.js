0fd3187edc99053f87bcef0fa57577b54db5e42b
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-05 11:39:29 +0100

    calculate and display in, out and interest summary

--- app/script.js
+++ app/script.js
@@ -83,6 +83,25 @@ const calcDisplayBalance = function(movements) {
 }
 calcDisplayBalance(account1.movements);
 
+const calcDisplaySummary = function(movements) {
+  const incomes = movements
+  .filter(mov => mov > 0)
+  .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(dep => dep * 1.2/ 100)
+    .filter(int => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+}
+calcDisplaySummary(account1.movements);
+
 // create usernames = owner initials lowercase
 const createUsernames = function(accs) {
   accs.forEach(function(acc) {
