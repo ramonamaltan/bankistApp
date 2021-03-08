7733e2306e0b55f28367724256f5025759baa9b9
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-08 09:38:43 +0100

    implementing smooth scrolling

--- app/home/script.js
+++ app/home/script.js
@@ -29,6 +29,28 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+////////////// IMPLEMENT SMOOTH SCROLLING //////////////////////
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e) {
+  const s1coords = section1.getBoundingClientRect();
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // })
+
+  // for modern browsers there's an easy way
+  section1.scrollIntoView({behavior: 'smooth'});
+})
+
+///////////////////////////////////////////////////////////////
 // NodeList
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
@@ -56,3 +78,32 @@ document
     message.remove();
     // message.parentElement.removeChild.message();
   })
+
+  // Style
+  message.style.backgroundColor = '#37383d';
+  message.style.width = '120%';
+
+  console.log(getComputedStyle(message).height);
+  message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 20 + 'px';
+
+  // document.documentElement.style.setProperty('--color-primary', 'orange');
+
+  // Attributes
+  const logo = document.querySelector('.nav__logo');
+  console.log(logo.alt);
+
+  console.log(logo.src); //http://localhost:8080/app/home/img/logo.png
+  console.log(logo.getAttribute('src')); //img/logo.png
+
+  const link = document.querySelector('.nav__link--btn');
+  console.log(link.href); // http://localhost:8080/app/home/#
+  console.log(link.getAttribute('href')); // #
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+// logo.classList.add();
+// logo.classList.remove();
+// logo.classList.toggle();
+// logo.classList.contains();
