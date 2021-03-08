7545e2b2bbc6311ca10acb1202b59fed9d94ef3e
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-08 15:56:28 +0100

    tabbed component

--- app/home/script.js
+++ app/home/script.js
@@ -66,6 +66,24 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
   }
 })
 
+/////////////////// Tabbed Component //////////////////////////
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // active tab
+  if (!clicked) return;
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+  
+  // active content area
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+})
+
 ///////////////////////////////////////////////////////////////
 // NodeList
 const allSections = document.querySelectorAll('.section');
