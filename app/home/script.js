846f7705d516dc314b92b6ebd93c19a77dc93758
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-08 16:52:00 +0100

    reveal elements/sections on scroll with using the observer API

--- app/home/script.js
+++ app/home/script.js
@@ -145,6 +145,31 @@ tabsContainer.addEventListener('click', function(e) {
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 })
 
+///////////////// Revealing Elements on Scroll ////////////////
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {root: null, threshold: 0.15})
+
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
+
+
+
+
+
+
+
+
 ///////////////////////////////////////////////////////////////
 // // NodeList
 // const allSections = document.querySelectorAll('.section');
