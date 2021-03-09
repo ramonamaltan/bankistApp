168f54963d10333bf9b06b7dffba5910122fe928
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-09 08:55:22 +0100

    added slider functionality

--- app/home/script.js
+++ app/home/script.js
@@ -10,7 +10,7 @@ const nav = document.querySelector('.nav')
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
-
+const dotContainer = document.querySelector('.dots');
 
 ///////////// Modal window ///////////////////////////////////////
 const openModal = function (e) {
@@ -165,7 +165,7 @@ allSections.forEach(function(section) {
 /////////////////// Lazy Loading Images //////////////////////
 const imgTargets = document.querySelectorAll('img[data-src]');
 
-const loadImg = function(entries, oberver) {
+const loadImg = function(entries, observer) {
   const [entry] = entries;
   if (!entry.isIntersecting) return
 
@@ -183,7 +183,73 @@ const imgObserver = new IntersectionObserver(loadImg, {root: null, threshold: 0,
 
 imgTargets.forEach(img => imgObserver.observe(img));
 
+////////////////// Slider Component /////////////////////////////
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
 
+const createDots = function() {
+  slides.forEach(function(_, i) {
+    dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`)
+  })
+}
+
+const activateDot = function(slide) {
+  document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+  document.querySelector(`.dots__dot[data-slide='${slide}`).classList.add('dots__dot--active');
+}
+
+const goToSlide = function(slide) {
+  slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`);
+}
+
+// change slides
+const nextSlide = function() {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+  activateDot(curSlide);
+}
+
+const prevSlide = function() {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+  activateDot(curSlide);
+}
+
+const init = function() {
+  goToSlide(0);
+  createDots();
+  activateDot(0);
+}
+
+init();
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+})
+
+dotContainer.addEventListener('click', function(e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const {slide} = e.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+})
 
 ///////////////////////////////////////////////////////////////
 // // NodeList
